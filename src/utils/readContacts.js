e24c6373d537fd9f97fee966bd6e34c835210468
Rybalkoyuliia <rybalkoyuliia4@gmail.com>
2025-06-01 21:33:21 -0400

    final logic

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,9 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import detect from 'detect-file-encoding-and-language';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  const { encoding } = await detect(PATH_DB);
+
+  return await JSON.parse(await fs.readFile(PATH_DB, encoding));
+};
